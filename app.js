3ab30708f63ee3f17c7b68f6e308bbea984694b0
mercedes <mercedesreynaga.mr@gmail.com>
2021-08-06 01:35:29 -0300

    se agrega registro y login parcial

--- app.js
+++ app.js
@@ -14,6 +14,15 @@ app.get('/', (req, res) => {
 app.get('/prueba', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/prueba.html'))
 })
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'))
+})
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'))
+})
+app.get('/recuperar', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/recuperarcontra.html'))
+})
 
 /* Servidor */
 app.listen(port, () => {
