33a80311453a99b8cda5eb83e8c318843c6d9828
MatiasGaleano <matii.galeano@gmail.com>
2021-08-30 22:12:55 -0300

    modificaciones en el head, json de productos y vistas

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,9 +1,18 @@
- let { getProducts } =require('../data/dataBase')
+ let { products } =require('../data/dataBase');
+ let productsTelevisores = products.filter(product => product.category === "Televisores")
+ let productsCelulares = products.filter(product => product.category === "Celulares")
+ let productsTablets = products.filter(product => product.category === "Tablets")
+ let productsGaming = products.filter(product => product.category === "Gaming")
 
 module.exports = {
     producto: (req, res) => {
       res.render('products', {
-          products : getProducts
+          productsTelevisores,
+          productsCelulares,
+          productsGaming,
+          productsTablets,
+          products
+
       })
     }
    
--- src/data/dataBase.js
+++ src/data/dataBase.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 module.exports = {
-    getProducts : JSON.parse(fs.readFileSync('./src/data/products.json', "utf-8")),
+    products : JSON.parse(fs.readFileSync('./src/data/products.json', "utf-8")),
     writeJson : (dataBase) => {
         fs.writeFileSync(`./src/data/products.json`, JSON.stringify(dataBase), "utf-8")
     }
