91f7912aa4d6c8021b3b8843879ccabd56142879
mercedes <mercedesreynaga.mr@gmail.com>
2021-11-03 21:57:26 -0300

    se agrega columna pais faltante en el address

--- src/database/models/Address.js
+++ src/database/models/Address.js
@@ -7,6 +7,9 @@ module.exports = function(sequelize, dataTypes){
             autoIncrement: true,
             allowNull: false 
         },
+        pais: {
+            type: dataTypes.STRING(100)
+        },
         province: {
             type: dataTypes.STRING(255)
         },
