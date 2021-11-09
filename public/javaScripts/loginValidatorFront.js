b85f7e6464f6fc00c8ef64b8888c9a1a06313dbb
mercedes <mercedesreynaga.mr@gmail.com>
2021-11-08 23:41:32 -0300

    se agregan cambios en  el accountsController y en el loginValidatorFront

--- public/javaScripts/loginValidatorFront.js
+++ public/javaScripts/loginValidatorFront.js
@@ -1,25 +1,23 @@
 window.onload=function() {
-    let emailValidate = document.querySelectorAll("email")
-    let $pass= document.querySelectorAll("pass")
-    emailValidate(input=>
+    let input = document.getElementById("email")
+  //  let $pass= document.querySelectorAll("pass")
+   
         input.addEventListener("blur",(event=>{
             
             emailValidate = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/.test(input.value)
 
-            if(input=="email"){
+          
                 if(emailValidate){
                 input.style.border = '3px rgba(14, 185, 90, 0.66) solid'
                     input.removeAttribute("error")
-                    return inputEmail = input.value
                 } else {
                     input.style.border = '3px solid rgba(185, 14, 14, 0.66)'
                     input.setAttribute("error", "error")
-                }
+                })
                 
                 
 
-            }
+  
         
         }
         
-    
