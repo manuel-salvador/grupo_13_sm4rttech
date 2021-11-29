9d479952e7dafe7264d96c9e4286aea11aa7e8da
MatiasGaleano <matii.galeano@gmail.com>
2021-11-29 12:28:42 -0300

    contador carrito

--- public/javaScripts/carritoDeCompra.js
+++ public/javaScripts/carritoDeCompra.js
@@ -1,17 +1,20 @@
 let counter = 1
-let $count = document.getElementsByClassName("count")
+let $count = document.getElementById('count');
 
-  function add () {
-    if(counter <= 10) {
-        counter = counter + 1
-        $count.innerHTML = `<p>${counter}</p>`
-    }
 
+let $menos = document.querySelector(".btn-subtract");
+$menos.addEventListener('click', function () {
+  if (counter > 0) {
+    counter = counter - 1
+    $count.innerHTML = `<p>${counter}</p>`
   }
+})
 
-  function subtract () {
-    if(counter > 0) {
-        counter = counter - 1
-        $count.innerHTML = `<p>${counter}</p>`
-    }
-  }
+
+let $suma = document.querySelector(".btn-add");
+$suma.addEventListener('click', function () {
+  if (counter <= 10) {
+    counter = counter + 1
+    $count.innerHTML = `<p>${counter}</p>`
+}
+})
--- src/controllers/carritoController.js
+++ src/controllers/carritoController.js
@@ -1,6 +1,20 @@
+const db = require('../database/models')
+const { Op } = require("sequelize");
+
+
 module.exports = {
     carrito: (req, res) => {
-        res.render('carritoDeCompra')
-    }
+        db.Product.findOne({
+              where: {
+                id: 1
+              },
+              include: [{ association: "category" }, { association: "colores" }, { association: "brand" },
+              { association: "capacities" }, { association: "images" }, { association: "rams" },
+              { association: "sizes" }]
+            })
+              .then(product => {
+                res.render('carritoDeCompra', { product })
+              })
+          },
     
 }
